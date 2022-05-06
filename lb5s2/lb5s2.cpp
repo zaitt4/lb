2967d73aa7a226b255de66db830198fa3c6e7c57
zaitt <90521247+zaitt4@users.noreply.github.com>
2022-05-06 17:36:08 +0300

    Create lb5s2.cpp

---
+++ lb5s2/lb5s2.cpp
@@ -0,0 +1,60 @@
+#include "Triangle.h"
+#include "Equilateral.h"
+#include <iostream>
+
+
+Triangle builtEq(Triangle& victim);
+
+
+int main() {
+
+	setlocale(LC_ALL, "ru");
+	Triangle obj(3,8,7);
+	Equilateral eqobj(2);
+
+	obj.ChangeSide(2, 'a');
+
+	eqobj.ChangeSide(3);
+
+	printf("Обьект типа Triangle:\n");
+	obj.show();
+
+	printf("\nОбъект типа Equilateral:\n");
+	eqobj.show();
+	printf("\n");
+	eqobj.ShowArea();
+	
+	Triangle newEq = builtEq(obj);
+
+	printf("\n\nПодстановка:\n");
+	newEq.show();
+  
+}
+
+
+Triangle builtEq(Triangle& victim) {
+
+	float a = victim['a'], b = victim['b'], c = victim['c'];
+
+	float factor = a;
+
+	if (a > b && a > c) {
+		
+		factor;
+	}
+
+	else if (b > a && b > c) {
+
+		factor = b;
+	}
+
+	else {
+
+		factor = c;
+	}
+
+	Equilateral nwEq(factor);
+	return nwEq;
+  
+}
+
